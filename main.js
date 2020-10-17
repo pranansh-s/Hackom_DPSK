279b22efe8146407b5f55fbf30207d4d2ad5e3a9
Pranansh Singh <43909197+pranansh-s@users.noreply.github.com>
2020-10-17 19:00:44 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const electron = require('electron');
-
-const {app, BrowserWindow} = electron;
-
-app.on('ready', function(){
-  let win = new BrowserWindow({fullscreen:true, webPreferences:{nodeIntegration:true}, frame:false});
-  win.loadFile('index.html')
-});
