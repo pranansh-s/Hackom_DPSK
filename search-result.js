854500a2ecf29693fb92ffcf249a7b216b202993
Pranansh Singh <43909197+pranansh-s@users.noreply.github.com>
2020-10-18 08:21:00 +0530

    apt

---
+++ search-result.js
@@ -0,0 +1,8 @@
+var btn = document.getElementById('search');
+btn.addEventListener("click", function(){
+  var type = document.getElementById('type');
+  if(type == "") return;
+
+  var locale = document.getElementById('locale');
+  var wh = document.getElementById('wh');
+});
