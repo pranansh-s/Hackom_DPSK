9ea4ee024acdbf84c6b1b818f7fd83846118bc1d
Pranansh Singh <43909197+pranansh-s@users.noreply.github.com>
2020-10-18 14:11:51 +0530

    lik

--- news.js
+++ news.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   var url = "https://newsapi.org/v2/top-headlines?q=corona&country=us&apiKey=492c83b3fb69467dba5345df7c3b8257";
 
     $.getJSON(url,function(data){
-      console.log(data);
       var li = document.getElementsByClassName("news");
       for(var i = 0; i < 5; ++i){
         li[i].style.backgroundImage = "url('" + data.articles[i].urlToImage + "')";
