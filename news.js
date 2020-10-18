571e078884573eb9986400d1038378108525d0c7
Pranansh Singh <43909197+pranansh-s@users.noreply.github.com>
2020-10-18 06:54:41 +0530

    apt

---
+++ news.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  var url = "http://newsapi.org/v2/top-headlines?q=corona&country=us&apiKey=492c83b3fb69467dba5345df7c3b8257";
+
+    $.getJSON(url,function(data){
+      console.log(data);
+      var li = document.getElementsByClassName("news");
+      for(var i = 0; i < 5; ++i){
+        li[i].style.backgroundImage = "url('" + data.articles[i].urlToImage + "')";
+        li[i].href = data.articles[i].url;
+        var h = document.createElement("h4");
+        h.textContent = data.articles[i].title;
+        li[i].appendChild(h);
+      }
+    })
+})
